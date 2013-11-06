87a62607ed071bf2b8e5607a98481607cd38f6f8
Taylor Braun-Jones <taylor@braun-jones.org>
2013-11-05 23:28:30 -0500

    Support for automatic -j MAKEFLAGS on OS X

--- profile.d/50make.sh
+++ profile.d/50make.sh
@@ -1,1 +1,5 @@
-export MAKEFLAGS=-j`grep -i -c ^processor /proc/cpuinfo`
+if [[ ${OSTYPE} =~ ^darwin ]]; then
+    export MAKEFLAGS=-j`sysctl -n hw.ncpu`
+else
+    export MAKEFLAGS=-j`grep -i -c ^processor /proc/cpuinfo`
+fi
