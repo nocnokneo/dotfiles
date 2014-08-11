d773c50af52ff0e0b2adc2fee09a057a438d9c8b
Taylor Braun-Jones <taylor@braun-jones.org>
2014-08-10 21:57:24 -0400

    Support ADT bundle on any platform

--- bashrc.d/50adt-bundle.sh
+++ bashrc.d/50adt-bundle.sh
@@ -1,6 +1,6 @@
 # Add tools and platform-tools directories to the system PATH if they exist
 # (and are not already in the system PATH)
-for toolpath in ~/adt-bundle-linux-x86_64/sdk/{platform-,}tools; do
+for toolpath in ~/adt-bundle-*/sdk/{platform-,}tools; do
     if [ -d $toolpath ] && ! echo $PATH | grep -q ${toolpath} ; then
         export PATH=${PATH}:${toolpath}
     fi
