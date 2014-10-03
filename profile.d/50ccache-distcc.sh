7b27a40a6655b65ee06786741132ac264ed96e5b
Taylor Braun-Jones <taylor@braun-jones.org>
2014-10-03 15:50:27 -0400

    Make ccache compliant with the XDG Base Directory Specification

--- profile.d/50ccache-distcc.sh
+++ profile.d/50ccache-distcc.sh
@@ -1,3 +1,6 @@
+# Make ccache compliant with the XDG Base Directory Specification
+export CCACHE_DIR=${XDG_CACHE_HOME:-$HOME/.cache}/ccache
+
 # Make the ccache objects CPU-specific by including /proc/cpuinfo when hashing
 # input files. This is necessary for objects compiled with `-march=native`
 export CCACHE_EXTRAFILES=/proc/cpuinfo
