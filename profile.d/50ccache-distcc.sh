b15d9d71cb2f2561ab06e04f007bcf950231009a
Taylor Braun-Jones <taylor@braun-jones.org>
2014-10-03 15:50:17 -0400

    More elegant way of making ccache support -march=native

--- profile.d/50ccache-distcc.sh
+++ profile.d/50ccache-distcc.sh
@@ -1,15 +1,6 @@
-# Force ccache dir to be CPU-specific (or at least architecture-specific)
-unset CCACHE_DIR CCACHE_UMASK
-# If we have gcc, use it to find out the CPU type so that the ccache objects
-# `-march=native` are portable accross machines. If we don't have gcc, at
-# least make the ccache architecture-specific.
-arch=$(uname -m)
-cpu=unknown
-if type -p gcc &>/dev/null; then
-    cpu=$(gcc -march=native -Q -v --help=target 2>/dev/null | awk '/ *-march=/ {print $2;}')
-fi
-export CCACHE_DIR=${HOME}/.ccache/${arch}_${cpu}
-mkdir -p ${CCACHE_DIR}
+# Make the ccache objects CPU-specific by including /proc/cpuinfo when hashing
+# input files. This is necessary for objects compiled with `-march=native`
+export CCACHE_EXTRAFILES=/proc/cpuinfo
 
 # Automatically use ccache and distcc for all gcc/cc/g++/etc... builds
 #export PATH="/usr/lib/ccache:$PATH"
