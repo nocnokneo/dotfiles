17261b7282a6b8bc77b8a31f67643c80a1f30238
Taylor Braun-Jones <taylor@braun-jones.org>
2014-10-20 18:19:34 -0400

    Fix ssh-agent breaks rsync

--- bashrc.d/50ssh-agent.sh
+++ bashrc.d/50ssh-agent.sh
@@ -2,10 +2,18 @@
 # distros for non-graphical logins). Unlock keys with `ssh-add`.
 #
 # From: http://mah.everybody.org/docs/ssh#run-ssh-agent
-SSH_AGENT=/usr/bin/ssh-agent
-SSH_AGENT_ARGS="-s"
-if [ -z "$SSH_AUTH_SOCK" -a -x "$SSH_AGENT" ]; then
-    eval `$SSH_AGENT $SSH_AGENT_ARGS`
-    # Kill ssh-agent when the shell dies
-    trap "kill $SSH_AGENT_PID" 0
-fi
+
+# Only launch ssh-agent for interactive shells because it only makes sense in
+# this scenario and because ssh-agent does not startup silently which breaks
+# rsync.
+case "$-" in
+    *i*)
+        SSH_AGENT=/usr/bin/ssh-agent
+        SSH_AGENT_ARGS="-s"
+        if [ -z "$SSH_AUTH_SOCK" -a -x "$SSH_AGENT" ]; then
+            eval `$SSH_AGENT $SSH_AGENT_ARGS`
+            # Kill ssh-agent when the shell dies
+            trap "kill $SSH_AGENT_PID" 0
+        fi
+        ;;
+esac
