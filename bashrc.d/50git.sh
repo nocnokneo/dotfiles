0643a0684951cb1273084f76918147e307429ba9
Taylor Braun-Jones <taylor@braun-jones.org>
2015-08-28 11:56:02 -0400

    Add hg-prompt to PS1 prompt

--- bashrc.d/50git.sh
+++ bashrc.d/50git.sh
@@ -6,11 +6,15 @@ if ! type __git_ps1 &>/dev/null; then
     . ~/.dotfiles/bashrc.d/git-prompt.sh
 fi
 
+hg_ps1() {
+    hg prompt "{ on {branch}}{ at {bookmark}}{status}" 2> /dev/null
+}
+
 if type __git_ps1 &>/dev/null; then
     GIT_PS1_SHOWDIRTYSTATE=1
     GIT_PS1_SHOWUNTRACKEDFILES=1
     GIT_PS1_SHOWUPSTREAM="auto"
-    export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[0;36m\]$(__git_ps1 " (%s)")\[\033[01;34m\] \$\[\033[00m\] '
+    export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[0;36m\]$(hg_ps1)$(__git_ps1 " (%s)")\[\033[01;34m\] \$\[\033[00m\] '
 fi
 
 function git-rev-number()
