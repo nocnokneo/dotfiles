6a9926037fc2e2263250c20f8d6dbdee3bb53b92
Taylor Braun-Jones <taylor@braun-jones.org>
2015-12-15 23:51:49 -0500

    Fix docker-{rm-exited,rmi-dangling} when there's nothing exited or dangling

--- bashrc.d/50docker.sh
+++ bashrc.d/50docker.sh
@@ -1,2 +1,2 @@
-alias docker-rm-exited="docker ps -q -f status=exited | xargs docker rm"
-alias docker-rmi-dangling="docker images -qf dangling=true | xargs docker rmi"
+alias docker-rm-exited="docker ps -q -f status=exited | xargs --no-run-if-empty docker rm"
+alias docker-rmi-dangling="docker images -qf dangling=true | xargs --no-run-if-empty docker rmi"
