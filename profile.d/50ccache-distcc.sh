f433bb7278eeb3ed579e4bbe99957e155635e4eb
Taylor Braun-Jones <taylor@braun-jones.org>
2018-08-15 11:24:17 -0400

    Stop using /proc/cpuinfo in ccache hash since it changes dynamically

--- profile.d/50ccache-distcc.sh
+++ profile.d/50ccache-distcc.sh
@@ -3,7 +3,9 @@ export CCACHE_DIR=${XDG_CACHE_HOME:-$HOME/.cache}/ccache
 
 # Make the ccache objects CPU-specific by including /proc/cpuinfo when hashing
 # input files. This is necessary for objects compiled with `-march=native`
-export CCACHE_EXTRAFILES=/proc/cpuinfo
+# TODO: Find alternative solution since this file can change dynamically (e.g.
+# the `cpu MHz` field)
+#export CCACHE_EXTRAFILES=/proc/cpuinfo
 
 # Automatically use ccache and distcc for all gcc/cc/g++/etc... builds
 #export PATH="/usr/lib/ccache:$PATH"
