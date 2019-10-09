3497445c3dee99c8bde98b311b95043ab0b68fe3
Taylor Braun-Jones <taylor@braun-jones.org>
2019-10-09 13:29:44 -0400

    PowerShell profile minor comments

--- conf/Microsoft.PowerShell_profile.ps1
+++ conf/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
+# Enable Emacs/Readline interactive mode
+# See: https://github.com/PowerShell/PSReadLine
 If($Host.Name -eq 'ConsoleHost') {
     Import-Module PSReadline
     Set-PSReadLineOption -EditMode Emacs
