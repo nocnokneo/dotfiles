235956904ccaad5838f1a840d4a68206ea4210cc
Taylor Braun-Jones <taylor@braun-jones.org>
2020-12-23 15:29:46 -0500

    Better qt creator plugin installation path

--- bin/upgrade-qtcreator-spellchecker.sh
+++ bin/upgrade-qtcreator-spellchecker.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-QTCREATOR_DIR=${HOME}/Qt/Tools/QtCreator
+qtcreator_version=$(qtcreator -version 2>&1 | egrep '^Qt Creator 4.14.0' | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' | head -n1)
 
-echo -e "Download the latest version of SpellChecker-Plugin into ~/Downloads\n\n  https://github.com/CJCombrink/SpellChecker-Plugin/releases/\n"
+PLUGIN_DIR=${HOME}/.local/share/data/QtProject/qtcreator/plugins/${qtcreator_version}
+mkdir -p ${PLUGIN_DIR}
+
+echo -e "Download the latest version of SpellChecker-Plugin for Qt Creator ${qtcreator_version} into ~/Downloads\n\n  https://github.com/CJCombrink/SpellChecker-Plugin/releases/\n"
 read -sp "Press enter when ready."
 echo -e "\n"
 package=$(find ~/Downloads/ -type f -name 'SpellChecker-Plugin_*' | xargs --no-run-if-empty ls -t | head -n1)
-echo "Installing ${package} into ${QTCREATOR_DIR}"
-tar -C ${QTCREATOR_DIR} --strip-components=1 -xf ${package}
+echo "Installing ${package} into ${PLUGIN_DIR}"
+tar -C ${PLUGIN_DIR} --strip-components=1 -xf ${package}
 echo Done.
