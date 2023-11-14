530b867d4cadb1da4e2918a7aec02f1d597d74a2
Taylor Braun-Jones <taylor@braun-jones.org>
2023-11-14 15:12:50 -0500

    Use starship to manage PS1

--- bashrc.d/50git.sh
+++ bashrc.d/50git.sh
@@ -2,17 +2,6 @@ if ! type git &>/dev/null; then
     return
 fi
 
-if ! type __git_ps1 &>/dev/null; then
-    . ~/.dotfiles/bashrc.d/git-prompt.sh
-fi
-
-if type __git_ps1 &>/dev/null; then
-    GIT_PS1_SHOWDIRTYSTATE=1
-    GIT_PS1_SHOWUNTRACKEDFILES=1
-    GIT_PS1_SHOWUPSTREAM="auto"
-    export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[0;36m\]$(__git_ps1 " (%s)")\[\033[01;34m\] \$\[\033[00m\] '
-fi
-
 function git-rev-number()
 {
     if [[ $1 == "" ]]; then
