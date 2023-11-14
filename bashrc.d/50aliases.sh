bb24c21b590018d19ff0d6ab2896548d8bc6a3d9
Taylor Braun-Jones <taylor@braun-jones.org>
2023-11-14 15:13:14 -0500

    ls with file size suffixs

--- bashrc.d/50aliases.sh
+++ bashrc.d/50aliases.sh
@@ -1,13 +1,13 @@
 # Enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
+    alias ls='ls --color=auto --human'
     #alias dir='dir --color=auto'
     #alias vdir='vdir --color=auto'
 fi
 
 # Some more ls aliases
-alias ll='ls -alF'
+alias ll='ls -alF --color=auto --human'
 alias la='ls -A'
 alias l='ls -CF'
 
