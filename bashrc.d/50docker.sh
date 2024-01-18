f300f2eef5e99a5aac4df6b5931ede5e2d05aa20
Taylor Braun-Jones <taylor@braun-jones.org>
2024-01-18 14:30:55 -0500

    docker-compose alias for podman

--- bashrc.d/50docker.sh
+++ bashrc.d/50docker.sh
@@ -2,3 +2,5 @@ for ociruntime in docker podman; do
     alias ${ociruntime}-rm-exited="${ociruntime} ps -q -f status=exited | xargs --no-run-if-empty ${ociruntime} rm"
     alias ${ociruntime}-rmi-dangling="${ociruntime} images -qf dangling=true | xargs --no-run-if-empty ${ociruntime} rmi"
 done
+
+alias docker-compose=podman-compose
