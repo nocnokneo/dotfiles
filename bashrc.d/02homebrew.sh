c05f1e7d5e61be0c4ea0e05c062f69988ff6c58e
Taylor Braun-Jones <taylor@braun-jones.org>
2024-03-18 13:47:05 -0400

    workaround linuxbrew MANPATH bug

--- bashrc.d/02homebrew.sh
+++ bashrc.d/02homebrew.sh
@@ -1,4 +1,4 @@
 if [[ -d /home/linuxbrew/.linuxbrew ]]; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    eval "MANPATH=$(manpath); $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     . /home/linuxbrew/.linuxbrew/etc/bash_completion.d/brew
 fi
