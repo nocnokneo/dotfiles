53e86bc07544d1fff4b51b5d2618cbd2ec9f7598
Taylor Braun-Jones <taylor@braun-jones.org>
2024-05-08 09:54:02 -0400

    auto add pulumi to PATH

--- bashrc.d/50pulumi.sh
+++ bashrc.d/50pulumi.sh
@@ -1,3 +1,8 @@
+# add Pulumi to the PATH
+if [ -x "${HOME}/.pulumi/bin/pulumi" ]; then
+    export PATH="$PATH:${HOME}/.pulumi/bin"
+fi
+
 if type pulumi &>/dev/null; then
     . <(pulumi gen-completion bash)
 fi
