502d68e13de93498b7022453b38cdcf91255c3da
Taylor Braun-Jones <taylor@braun-jones.org>
2024-05-08 09:54:50 -0400

    podman/docker fixes

--- bashrc.d/50docker.sh
+++ bashrc.d/50docker.sh
@@ -1,6 +1,9 @@
 for ociruntime in docker podman; do
     alias ${ociruntime}-rm-exited="${ociruntime} ps -q -f status=exited | xargs --no-run-if-empty ${ociruntime} rm"
     alias ${ociruntime}-rmi-dangling="${ociruntime} images -qf dangling=true | xargs --no-run-if-empty ${ociruntime} rmi"
+    alias ${ociruntime}-cleanup="${ociruntime} system prune --filter 'until=24h'"
 done
 
-alias docker-compose=podman-compose
+if type podman-compose > /dev/null && ! type docker-compose > /dev/null; then
+    alias docker-compose=podman-compose
+fi
