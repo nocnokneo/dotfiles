8efee609b81694a690840d1ff4a67342614f8039
Taylor Braun-Jones <taylor@braun-jones.org>
2025-06-05 15:53:53 +0000

    ssh-agent

--- bashrc.d/50ssh-agent.sh
+++ bashrc.d/50ssh-agent.sh
@@ -3,17 +3,17 @@
 #
 # From: http://mah.everybody.org/docs/ssh#run-ssh-agent
 
+_ensure_ssh_agent() {
+    if [[ -z "$SSH_AUTH_SOCK" ]] && which ssh-agent >&/dev/null; then
+        eval `ssh-agent -s`
+        # Kill ssh-agent when the shell dies
+        trap "kill $SSH_AGENT_PID" 0
+    fi
+}
+
 # Only launch ssh-agent for interactive shells because it only makes sense in
 # this scenario and because ssh-agent does not startup silently which breaks
 # rsync.
-case "$-" in
-    *i*)
-        SSH_AGENT=/usr/bin/ssh-agent
-        SSH_AGENT_ARGS="-s"
-        if [ -z "$SSH_AUTH_SOCK" -a -x "$SSH_AGENT" ]; then
-            eval `$SSH_AGENT $SSH_AGENT_ARGS`
-            # Kill ssh-agent when the shell dies
-            trap "kill $SSH_AGENT_PID" 0
-        fi
-        ;;
-esac
+if [[ $- == *i* ]]; then
+    _ensure_ssh_agent
+fi
