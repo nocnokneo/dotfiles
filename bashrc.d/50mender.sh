a3b4eca856c30eb253da16e95fefda343357629f
Taylor Braun-Jones <taylor@braun-jones.org>
2025-11-10 21:32:12 -0500

    Silence mender stdout when starting non-login bash shell

--- bashrc.d/50mender.sh
+++ bashrc.d/50mender.sh
@@ -1,6 +1,6 @@
 
 if type mender-cli &>/dev/null; then
-    if mender-cli | grep -s 'Configuration file not found' &>/dev/null; then
+    if mender-cli 2>/dev/null | grep -s 'Configuration file not found' &>/dev/null; then
         echo "{}" > ${HOME}/.mender-clirc
     fi
     . <(mender-cli completion bash 2>/dev/null)
