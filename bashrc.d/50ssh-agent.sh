ccb0a74ab7d582256a6f795765e0e9468c59c74e
Taylor Braun-Jones <taylor@braun-jones.org>
2026-01-29 09:31:38 -0500

    Use improved ssh-agent launching from VS Code docs

--- bashrc.d/50ssh-agent.sh
+++ bashrc.d/50ssh-agent.sh
@@ -1,19 +1,15 @@
 # Start ssh-agent if it is not already running (needed on most (all?)
 # distros for non-graphical logins). Unlock keys with `ssh-add`.
 #
-# From: http://mah.everybody.org/docs/ssh#run-ssh-agent
+# From: https://code.visualstudio.com/remote/advancedcontainers/sharing-git-credentials#_using-ssh-keys
 
-_ensure_ssh_agent() {
-    if [[ -z "$SSH_AUTH_SOCK" ]] && which ssh-agent >&/dev/null; then
-        eval `ssh-agent -s`
-        # Kill ssh-agent when the shell dies
-        trap "kill $SSH_AGENT_PID" 0
-    fi
-}
-
-# Only launch ssh-agent for interactive shells because it only makes sense in
-# this scenario and because ssh-agent does not startup silently which breaks
-# rsync.
-if [[ $- == *i* ]]; then
-    _ensure_ssh_agent
+if [ -z "$SSH_AUTH_SOCK" ]; then
+   # Check for a currently running instance of the agent
+   RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+   if [ "$RUNNING_AGENT" = "0" ]; then
+        # Launch a new instance of the agent
+        ssh-agent -s &> $HOME/.ssh/ssh-agent
+   fi
+   eval `cat $HOME/.ssh/ssh-agent` > /dev/null
+   ssh-add 2> /dev/null
 fi
